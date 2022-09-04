23a2920adf56be4427dd826c763f47aee01fb33c
Elvys Marinho <elvysnascimentoevs@gmail.com>
2022-09-04 18:02:28 -0300

    Response entity + if/else

--- src/main/java/com/project/professor/allocation/controller/ProfessorController.java
+++ src/main/java/com/project/professor/allocation/controller/ProfessorController.java
@@ -1,18 +1,24 @@
 package com.project.professor.allocation.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.professor.allocation.entity.Department;
+import com.project.professor.allocation.entity.Professor;
+import com.project.professor.allocation.service.DepartmentService;
 import com.project.professor.allocation.service.ProfessorService;
 
 @RestController
+@RequestMapping(path = "/departments")
 public class ProfessorController {
-	
+
 	private final ProfessorService professorService;
 
 	public ProfessorController(ProfessorService professorService) {
 		super();
 		this.professorService = professorService;
 	}
-	
 
 }
--- src/main/java/com/project/professor/allocation/service/DepartmentService.java
+++ src/main/java/com/project/professor/allocation/service/DepartmentService.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Service;
 import com.project.professor.allocation.entity.Department;
 import com.project.professor.allocation.repository.DepartmentRepository;
 
-//INJEÇÃO DE DEPENDENCIA
-@Service // SEMPRE FAZER AS ANOTAÇÕES COM @
+@Service
 public class DepartmentService {
 
 	private final DepartmentRepository departmentRepository;
@@ -17,10 +16,32 @@ public class DepartmentService {
 		super();
 		this.departmentRepository = departmentRepository;
 	}
-	
-	public List<Department> findAll(){
-		return departmentRepository.findAll();
-		
+
+	public List<Department> findByNameContaining(String name) {
+		return departmentRepository.findByNameContaining(name);
+
 	}
-	
+
+	public Department findById(Long id) {
+		return departmentRepository.findById(id).orElse(null);
+	}
+
+	public List<Department> findAll() {
+		return departmentRepository.findAll();
+	}
+
+	public Department save(Department department) {
+		department.setId(null);
+		return departmentRepository.save(department);
+	}
+
+	public void deleteById(Long id) {
+
+		if (departmentRepository.existsById(id)) {
+
+			departmentRepository.deleteById(id);
+		}
+
+	}
+
 }
